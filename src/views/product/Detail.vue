8e2a968aef5bc94ada0ac2f41e8f4294b0fda6ec
Japar12 <abduljapar84@gmail.com>
2024-04-28 18:27:06 +0700

    first commit

---
+++ src/views/product/Detail.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div id="page-wrap" v-if="product">
+      <div id="img-wrap">
+        <img :src="`http://localhost:8000${product.imageUrl}`" alt="" />
+      </div>
+      <div id="product-details">
+        <h1>{{ product.name }}</h1>
+        <h3 id="price">Rp{{ product.price }}</h3>
+        <p>Average rating: {{ product.averageRating }}</p>
+        <button id="add-to-cart" @click="addToCart(product.code)">Add to Cart</button>
+        <p>{{ product.description }}</p>
+      </div>
+    </div>
+    <NotFound v-else />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import NotFound from "../errors/404.vue";
+
+export default {
+  components: {
+    NotFound,
+  },
+  data() {
+    return {
+      product: {},
+    };
+  },
+  methods: {
+    async addToCart(product) {
+      await axios.post("http://localhost:8000/api/orders/update/user/1", {
+        product: product,
+      });
+    },
+  },
+
+  async created() {
+    const code = this.$route.params.id;
+    const result = await axios.get(`
+        http://localhost:8000/api/products/${code}`);
+    this.product = result.data;
+  },
+};
+</script>
+
+<style scoped>
+#page-wrap {
+  margin-top: 16px;
+  padding: 16px;
+  max-width: 600px;
+}
+
+#img-wrap {
+  text-align: center;
+}
+
+img {
+  width: 400px;
+}
+
+#product-details {
+  padding: 16px;
+  position: relative;
+}
+
+#add-to-cart {
+  width: 100%;
+}
+
+#price {
+  position: absolute;
+  top: 24px;
+  right: 16px;
+}
+</style>
